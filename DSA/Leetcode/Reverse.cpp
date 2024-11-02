3882a3aba8f71c7a35ae8088a31b72a8ea4d0cbb
Kalyanipagar <159225439+kalyanipagar18@users.noreply.github.com>
2024-11-02 20:14:04 +0530

    Create Reverse.cpp

---
+++ DSA/Leetcode/Reverse.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int reverse(int x) {
+      int revnum=0;
+
+      while(x!=0)
+      {
+        int dig=x%10;
+        if(revnum>INT_MAX/10 || revnum< INT_MIN/10)
+        revnum=revnum*10+ dig;
+        x=x/10;
+    
+      }  
+      return revnum;
+    }
+};
